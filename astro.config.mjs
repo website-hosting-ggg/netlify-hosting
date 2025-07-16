946cc1188074f619fb5780f8ca3bbd81a79225c3
GGG <website.hosting.ggg@gmail.com>
2025-07-17 00:47:24 +0200

    fix2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/static';
 
 export default defineConfig({
-  output: 'static',
-  adapter: staticAdapter(),
+  // No adapter needed for static export (default in Astro)
 });
